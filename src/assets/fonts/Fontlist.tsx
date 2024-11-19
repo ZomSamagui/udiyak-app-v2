940d1d0ac426702c87d2c04d490c6959c74cee72
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-11-19 14:31:30 +0900

    📰 :: set Font

---
+++ src/assets/fonts/Fontlist.tsx
@@ -0,0 +1,6 @@
+export const Fontlist = {
+    'STUNNING' : require('./STUNNINGOTF.otf'),
+    'Jamsil-Regular' : require('./TheJamsilRegular.otf'),
+    'Jamsil-Medium' : require('./TheJamsilMedium.otf'),
+    'Jamsil-Bold' : require('./TheJamsilBold.otf'),
+}
