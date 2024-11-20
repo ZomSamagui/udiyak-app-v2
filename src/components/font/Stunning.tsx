28a653080c1bd1ee24eaf0bdaf7da994589a1a30
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-11-20 14:45:12 +0900

    📰 :: add Font

---
+++ src/components/font/Stunning.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Text, StyleSheet, ActivityIndicator} from "react-native";
+import {useFonts} from "expo-font";
+
+type StunningTextProps = {
+    children: React.ReactNode;
+    style?: any;
+    fontSize?: number;
+};
+
+export default function StunningText({children, style, fontSize}: StunningTextProps) {
+    const [fontsLoaded] = useFonts({
+        Stunning: require('../../assets/fonts/STUNNINGOTF.otf')
+    })
+
+    if (!fontsLoaded) return <ActivityIndicator size="large" color="black"/>;
+
+    return (
+        <Text style={[style, {fontFamily: `Stunning`, fontSize: fontSize}]}>
+            {children}
+        </Text>
+    )
+}
---
+++ src/components/font/TheJamsil.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Text, StyleSheet, ActivityIndicator} from "react-native";
+import {useFonts} from "expo-font";
+
+type TheJamsilTextProps = {
+    children: React.ReactNode;
+    style?: any;
+    fontSize?: number;
+    fontWeight?: "Regular" | "Medium" | "Bold";
+};
+
+export default function TheJamsilText({children, style, fontSize, fontWeight = `Bold`}: TheJamsilTextProps) {
+    const [fontsLoaded] = useFonts({
+        TheJamsilBold: require('../../assets/fonts/TheJamsilBold.otf'),
+        TheJamsilMedium: require('../../assets/fonts/TheJamsilMedium.otf'),
+        TheJamsilRegular: require('../../assets/fonts/TheJamsilRegular.otf'),
+    });
+
+    if (!fontsLoaded) return <ActivityIndicator size="large" color="#0000ff"/>;
+
+    return (
+        <Text style={[style, {fontFamily: `TheJamsil${fontWeight}`, fontSize: fontSize}]}>
+            {children}
+        </Text>
+    )
+}
