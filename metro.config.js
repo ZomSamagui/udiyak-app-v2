587d5f0595a75f0365d325bc3d7edf628a202c62
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-11-22 21:15:35 +0900

    🚑 :: 버전 충돌 해결

--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,18 @@ const { getDefaultConfig } = require('@react-native/metro-config');
 module.exports = (async () => {
     const defaultConfig = await getDefaultConfig(__dirname);
 
-    defaultConfig.resolver.assetExts = [
-        ...defaultConfig.resolver.assetExts,
-        'png',  // 명시적으로 png 추가
-        'jpg',
-        'jpeg',
-        'svg',
-        'otf'
-    ];
+    defaultConfig.transformer = {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    };
+
+    defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
 
     defaultConfig.resolver.sourceExts = [
         ...defaultConfig.resolver.sourceExts,
         'svg'
     ];
+
+    defaultConfig.resolver.assetExts.push('otf', 'png', 'jpg', 'jpeg');
 
     return defaultConfig;
 })();
