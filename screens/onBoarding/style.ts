20599f7e44005cd2351d0c936779435f8a9d9729
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-11-22 21:16:30 +0900

    🚧 :: OnBoarding...

---
+++ screens/onBoarding/style.ts
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        backgroundColor: '#fff',
+        paddingTop: 130,
+        paddingLeft: 40,
+    },
+    subContainer: {
+        marginTop: 12,
+    },
+    subTitle: {
+        color: '#D9D9D9',
+        marginTop: 5,
+    },
+});
+
+export default styles;
