b32e9431b2ef9002827a7d558f4dccede2d68e62
박상민 <a01071397193@gmail.com>
2024-11-28 12:01:40 +0900

    ♿️ :: Button 컴포넌트화

---
+++ components/button/index.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {Thema} from "style/thema";
+import TheJamsilText from "components/fonts/TheJamsil";
+
+type ButtonProps = {
+    title?: string;
+    onPress?: () => void;
+};
+
+export const UdiyakButton = ({title = "Text", onPress}: ButtonProps) => {
+    const [isPressed, setIsPressed] = useState(false);
+
+    return (
+        <TouchableOpacity
+            style={[styles.buttonContainer, isPressed && styles.buttonPressed]}
+            onPressIn={() => setIsPressed(true)}
+            onPressOut={() => setIsPressed(false)}
+            onPress={onPress}
+        >
+            <TheJamsilText fontSize={18} fontWeight={"Medium"} style={styles.buttonText}>
+                {title}
+            </TheJamsilText>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        backgroundColor: Thema.colors.primary[400],
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        width: Thema.width.large,
+        height: Thema.height.large,
+        borderRadius: Thema.borderRadius.primary,
+        elevation: 5,
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonPressed: {
+        backgroundColor: Thema.colors.primary[500],
+    },
+    buttonText: {
+        color: Thema.colors.white,
+        fontSize: 16,
+        textAlign: "center",
+    },
+});
