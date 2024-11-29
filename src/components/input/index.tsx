8b4bc88bfb8eb76cbb306748cbcb9ff46a9c3d7c
박상민 <a01071397193@gmail.com>
2024-11-29 10:29:27 +0900

    ⚡️ :: keyBoardType, returnText, Password 블러 처리

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,23 +1,29 @@
 import React, {useState} from "react";
-import {View, StyleSheet, TextInput, UIManager, LayoutAnimation} from "react-native";
+import {View, StyleSheet, TextInput,} from "react-native";
 
 import {Thema} from "src/style/thema";
-import {Platform} from "expo-modules-core";
 
 type InputProps = {
     placeholder?: string;
     value?: string;
     onChangeText?: (text: string) => void;
+    securePassword?: boolean;
+    keyboardType?: "default" | "email-address" | "visible-password";
+    returnKeyType?: '완료' | '다음' | '검색하기';
 };
 
-export const UdiyakInput = ({placeholder, value, onChangeText}: InputProps) => {
+export const UdiyakInput = ({
+                                placeholder, value, onChangeText, keyboardType = 'default',
+                                returnKeyType = '완료', securePassword = false,
+                            }: InputProps) => {
     const [isFocused, setIsFocused] = useState(false);
 
     return (
         <View style={styles.container}>
             <TextInput
                 style={[
-                    [styles.inputContainer],
+                    styles.inputContainer,
+                    {fontSize: Thema.fontSize.body, color: Thema.colors.gray["300"],},
                     isFocused ? styles.focused : styles.unfocused,
                 ]}
                 placeholder={placeholder}
