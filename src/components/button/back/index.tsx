c6a90c3e541a04aeb94ab6fe0eb1e2829bc75608
박상민 <a01071397193@gmail.com>
2024-11-29 10:42:35 +0900

    ♿️ :: 외부에서 추가로 스타일을 적용할 때 병합되도록 수정

--- src/components/button/back/index.tsx
+++ src/components/button/back/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, ViewStyle} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
 import {Thema} from "src/style/thema";
 
-const BackButton = () => {
+type BackButtonProps = {
+    styles?: ViewStyle;
+    onPress?: () => void;
+}
+
+const BackButton = ({styles, onPress}: BackButtonProps) => {
     const navigation = useNavigation();
 
+    // 기본 스타일과 전달받은 스타일을 병합하고, 전달받은 스타일이 없으면 기본 스타일만 사용하는 코드
+    const combinedStyles = StyleSheet.flatten([defaultStyles.backButton, styles || {}]);
+
     return (
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <TouchableOpacity style={combinedStyles} onPress={onPress || (() => navigation.goBack())}>
             <Feather name="arrow-left" size={24} color="black"/>
         </TouchableOpacity>
     );
 };
 
-const styles = StyleSheet.create({
+const defaultStyles = StyleSheet.create({
     backButton: {
         color: Thema.colors.gray[300],
         position: 'absolute',
