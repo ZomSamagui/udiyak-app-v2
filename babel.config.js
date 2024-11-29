cc8c2c151d27f3dd75c35c7f63025053a85d9393
박상민 <a01071397193@gmail.com>
2024-11-29 12:03:40 +0900

    🐛 :: help depandabot

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'], // Combine presets into one array
+        presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
         plugins: [
             [
                 'module-resolver',
