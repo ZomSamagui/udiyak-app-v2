b777ba427b67548da416347cd8e940cf3792fc4c
박상민 <a01071397193@gmail.com>
2024-11-29 09:34:00 +0900

    📰 :: add medicine type

---
+++ src/type/medicine.type.ts
@@ -0,0 +1,8 @@
+export interface medicineProps {
+    id: number;
+    name: string;
+    usage: string; // 사용 용도
+    efficacy: string; // 효능 효과
+    dosage: string; // 복용 방법
+    precautions: string; // 주의 사
+}
