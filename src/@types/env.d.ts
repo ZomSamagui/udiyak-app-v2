852c762785e4d91e1b17ee5d941804d4714b5b80
박상민 <a01071397193@gmail.com>
2024-12-01 06:11:05 +0900

    🚀 :: apy key 추가

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
     export const SERVER_URL: string;
-    export const GOOGLE_MAP_API_KEY: string
+    export const GOOGLE_MAP_API_KEY: string;
+    export const MEDICINES_API_KEY: string;
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,20 @@
-import dotenv from 'dotenv';
-import {SERVER_URL} from "react-native-dotenv";
-
-dotenv.config();
+import { SERVER_URL, GOOGLE_MAP_API_KEY, MEDICINES_API_KEY } from "@env";
 
 interface Config {
-    serverUrl: string;
+    SERVER_URL: string;
+    GOOGLE_MAP_API_KEY: string;
+    MEDICINES_API_KEY: string;
 }
 
-const serverUrl = process.env.SERVER_URL;
 
-if (!serverUrl) throw new Error('서버 주소를 찾을 수 없음.');
+if (!SERVER_URL) throw new Error('서버 주소를 찾을 수 없음.');
+if (!GOOGLE_MAP_API_KEY) throw new Error('구글 API 에러');
+if (!MEDICINES_API_KEY) throw new Error('약 API 에러');
 
 const config: Config = {
-    serverUrl,
-}
+    SERVER_URL,
+    GOOGLE_MAP_API_KEY,
+    MEDICINES_API_KEY,
+};
 
 export default config;
