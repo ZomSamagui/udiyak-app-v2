fb40dfa15a59a128b153609321e4586efc3f9292
박상민 <a01071397193@gmail.com>
2024-12-02 02:29:16 +0900

    📝 :: GOOGLE -> KAKAO

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,5 +1,4 @@
 declare module '@env' {
     export const SERVER_URL: string;
-    export const GOOGLE_MAP_API_KEY: string;
-    export const MEDICINES_API_KEY: string;
+    export const KAKAO_REST_API_KEY: string;
 }
