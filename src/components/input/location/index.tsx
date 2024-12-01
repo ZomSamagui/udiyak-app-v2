62f387406a3c8c93d560fe6b53a33016c2b5405e
박상민 <a01071397193@gmail.com>
2024-12-02 06:00:34 +0900

    🔨 :: 도로명으로도 검색 가능하게 수정

--- src/components/input/location/index.tsx
+++ src/components/input/location/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextInput, Button, View } from 'react-native';
-import {Thema} from "../../../style/thema";
+import { Thema } from "src/style/thema";
 
 interface LocationInputProps {
     onSearch: (query: string) => void;
@@ -11,15 +11,15 @@ const LocationInput: React.FC<LocationInputProps> = ({ onSearch }) => {
 
     const handleSearch = () => {
         if (query.trim()) {
-            onSearch(query);
+            onSearch(query);  // 입력된 텍스트를 onSearch 함수에 전달
         }
     };
 
     return (
         <View style={{ padding: 10 }}>
             <TextInput
-                style={{ width:"100%", height: 50, borderRadius: 5, borderColor: Thema.colors.gray[200], borderWidth: 1, marginBottom: 10, padding: 10 }}
-                placeholder="장소를 입력하세요"
+                style={{ width: "100%", height: 50, borderRadius: 5, borderColor: Thema.colors.gray[200], borderWidth: 1, marginBottom: 10, padding: 10 }}
+                placeholder="장소 또는 도로명을 입력하세요"
                 value={query}
                 onChangeText={setQuery}
             />
